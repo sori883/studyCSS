8a180a2d31fc887acf9be525e98db672b5927044
sori883 <service@dlme.biz>
2019-07-27 21:27:27 +0900

    sassのコンパイル、autoflefixer、圧縮が出来るようになったよ！！ やったねたえちゃん

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,16 +1,7 @@
-'use strict'
-
-module.exports = ctx => ({
-  map: ctx.file.dirname.includes('examples') ?
-    false :
-    {
-      inline: false,
-      annotation: true,
-      sourcesContent: true
-    },
-  plugins: {
-    autoprefixer: {
-      cascade: false
-    }
+module.exports = {
+    plugins: [
+      require('autoprefixer')({
+        cascade: false
+      })
+    ]
   }
-})
