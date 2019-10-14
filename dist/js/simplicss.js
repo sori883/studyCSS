8ccc4c59e530f5b93c1a51d431bfb76777664c49
sori883 <service@dlme.biz>
2019-10-14 15:01:27 +0900

    文法ミスを修正

--- dist/js/simplicss.js
+++ dist/js/simplicss.js
@@ -1,14 +1,9 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('jquery')) :
-  typeof define === 'function' && define.amd ? define(['jquery'], factory) :
-  (global = global || self, factory(global.jQuery));
-}(this, function ($) { 'use strict';
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}(function () { 'use strict';
 
-  $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
-  $(document).ready(function () {
-    $(".scroll").smoothScroll();
-  });
 
 }));
 //# sourceMappingURL=simplicss.js.map
